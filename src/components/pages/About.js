60712be5280dd275286ae94184c0035fe900c618
Nodirbek Mamadaliyev <nodirbekmamadaliyev@gmail.com>
2020-11-27 14:19:00 +0300

    user

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home =()=> {
+    return (
+        <div className="container">
+            <div className="py-4">
+                <h1>About Page</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Home =()=> {
+    return (
+        <div className="container">
+            <div className="py-4">
+                <h1>Contact Page</h1>
+                <form action="nodir/user/login" method="post">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="text" class="form-control" name="username" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" name="password" id="exampleInputPassword1" />
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Home
